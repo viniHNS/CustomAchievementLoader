6752240ef9ea20845e89f12361252cfc3343637c
Cj <161484149+CJ-SPT@users.noreply.github.com>
2024-08-09 04:35:15 -0400

    first commit

---
+++ src/mod.ts
@@ -0,0 +1,126 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { DependencyContainer } from "tsyringe";
+
+import fs from 'fs';
+import path from "node:path";
+
+import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
+import { IDatabaseTables } from "@spt/models/spt/server/IDatabaseTables";
+import { IPreSptLoadMod } from "@spt/models/external/IPreSptLoadMod";
+import { InstanceManager } from "./InstanceManager";
+import { LogTextColor } from "@spt/models/spt/logging/LogTextColor";
+import { IAchievement } from "@spt/models/eft/common/tables/IAchievement";
+
+class CustomAchievementLoader implements IPostDBLoadMod, IPreSptLoadMod
+{
+    private InstanceManager: InstanceManager = new InstanceManager();
+    private Database: IDatabaseTables;
+
+    public preSptLoad(container: DependencyContainer)
+    {
+        this.InstanceManager.preSptLoad(container, "Custom Achievement Loader");
+    }
+
+    public postDBLoad(container: DependencyContainer): void
+    {
+        this.InstanceManager.postDBLoad(container);
+        this.Database = this.InstanceManager.database;
+     
+        this.InstanceManager.logger.logWithColor("CJCAL: Loading achievement files", LogTextColor.GREEN);
+
+        this.importAchievementData();
+        this.loadAchievementLocales();
+        this.loadImages();
+    }
+
+    private importAchievementData(): void
+    {
+        const dataPath = path.join(path.dirname(__filename), "..", "db", "data");
+        const files = fs.readdirSync(dataPath);
+        
+        const jsonFiles = files
+            .filter(file => path.extname(file) === ".json")
+            .map(file => path.basename(file, ".json"));
+
+        const achievmentTable = this.Database.templates.achievements;
+
+        for (const file of jsonFiles)
+        {   
+            const filePath = path.join(path.dirname(__filename), "..", "db", "data", `${file}.json`);
+            const data = this.loadAchievementFile(filePath);
+
+            for (const achievement of data)
+            {
+                achievmentTable.push(achievement);
+            }
+
+            this.InstanceManager.logger.logWithColor(`CJCAL: Loaded: ${data.length} achievements from ${file}.json`, LogTextColor.GREEN);
+        }    
+    }
+
+    private loadAchievementLocales(): void
+    {
+        const localesPath = path.join(path.dirname(__filename), "..", "db", "locales");
+        const subDirs = fs.readdirSync(localesPath);
+
+        for (const lang of subDirs)
+        {
+            const langDir = path.join(path.dirname(__filename), "..", "db", "locales", lang);
+            const localeFiles = fs.readdirSync(langDir);
+
+            for (const file of localeFiles)
+            {
+                const localeData = this.loadStringDictionary(path.join(langDir, file));
+
+                this.importLocaleData(lang, localeData);
+            }
+        }
+    }
+
+    private importLocaleData(lang: string, localeData: Record<string, string>): void
+    {
+        const globalLocales = this.Database.locales.global;
+
+        for (const entry in localeData)
+        {
+            console.log(entry);
+
+            globalLocales[lang][entry] = localeData[entry];
+        }
+    }
+
+    private loadAchievementFile(filePath: string): IAchievement[] 
+    {
+        const data = fs.readFileSync(filePath, 'utf-8');
+        const jsonData = JSON.parse(data) as IAchievement[];
+        return jsonData;
+    }
+
+    private loadStringDictionary(filePath: string): Record<string, string> 
+    {
+        const data = fs.readFileSync(filePath, 'utf-8');
+        const jsonData = JSON.parse(data) as Record<string, string>;
+        return jsonData;
+    }
+
+    private loadImages(): void
+    {
+        const imagesPath = path.join(path.dirname(__filename), "..", "db", "images");
+        const images = fs.readdirSync(imagesPath);
+
+        const imageRouter = this.InstanceManager.imageRouter;
+
+        for (const image of images)
+        {
+            const imagePath = path.join(imagesPath, image);
+            const filenameWithoutExtension = path.basename(imagePath, path.extname(imagePath));
+            
+            console.log(imagePath);
+            console.log(filenameWithoutExtension);
+
+            imageRouter.addRoute(`/files/achievement/${filenameWithoutExtension}`, imagePath);
+        }
+    }
+}
+
+export const mod = new CustomAchievementLoader();
---
+++ types/models/eft/ragfair/IRagfairOffer.d.ts
@@ -0,0 +1,45 @@
+import { Item } from "@spt/models/eft/common/tables/IItem";
+import { MemberCategory } from "@spt/models/enums/MemberCategory";
+export interface IRagfairOffer {
+    sellResult?: SellResult[];
+    _id: string;
+    items: Item[];
+    requirements: OfferRequirement[];
+    root: string;
+    intId: number;
+    /** Handbook price */
+    itemsCost: number;
+    /** Rouble price per item */
+    requirementsCost: number;
+    startTime: number;
+    endTime: number;
+    sellInOnePiece: boolean;
+    /** Rouble price - same as requirementsCost */
+    summaryCost: number;
+    user: IRagfairOfferUser;
+    /** Trader only */
+    unlimitedCount?: boolean;
+    loyaltyLevel: number;
+    buyRestrictionMax?: number;
+    buyRestrictionCurrent?: number;
+    locked?: boolean;
+}
+export interface OfferRequirement {
+    _tpl: string;
+    count: number;
+    onlyFunctional: boolean;
+}
+export interface IRagfairOfferUser {
+    id: string;
+    nickname?: string;
+    rating?: number;
+    memberType: MemberCategory;
+    selectedMemberCategory?: MemberCategory;
+    avatar?: string;
+    isRatingGrowing?: boolean;
+    aid?: number;
+}
+export interface SellResult {
+    sellTime: number;
+    amount: number;
+}
